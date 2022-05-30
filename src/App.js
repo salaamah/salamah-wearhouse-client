987dbfdbe90ed5d1e329fe157e158c2fcc0a61e4
salaamah <muhammadsalamatullah@gmail.com>
2022-05-30 20:36:16 +0300

    wearhouse client site created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/items')
+    .then(res=>res.json())
+    .then(data=>setItems(data));
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Salamah Grocery Wearhouse!</h1>
+      <h1>Number of Items: {items.length}</h1>
     </div>
   );
 }
